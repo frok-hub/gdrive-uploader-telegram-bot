647f75118f5a21a36f2c0ce72d5b1ca737a326cc
frok-hub <69907864+frok-hub@users.noreply.github.com>
2020-09-08 10:57:43 +0530

    Update uploader.py

--- plugins/uploader.py
+++ plugins/uploader.py
@@ -32,7 +32,7 @@ async def upload_file(creds, file_path, filesize, parent_id, message):
     await message.edit(f'📤 **Uploading...**\n**Filename:** ```{file_name}```\n**Size:** ```{filesize}```')
     body = {
         "name": file_name,
-        "description": "Uploaded using @UploadGdriveBot",
+        "description": "Uploaded using @s4h_gdriveitbot",
         "mimeType": mime_type,
     }
     if parent_id:
